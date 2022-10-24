8e833ea1b550247dc495e69d85b4b3d31f4cd134
Marc Vidal <Marc Vidal Ardevol>
2022-10-24 15:14:50 +0200

    FUNCIONA LOCAL

--- public/index.php
+++ public/index.php
@@ -5,11 +5,20 @@
 
 
     /* ----- CONTROLERS ----- */
+<<<<<<< HEAD
     // require_once "../src/2-controls/adminControl.php";
     // require_once "../src/2-controls/mainControl.php";
     // require_once "../src/2-controls/profileControl.php";
     // require_once "../src/2-controls/signinControl.php";
     // require_once "../src/2-controls/signupControl.php";
+=======
+    require_once "../src/2-controls/adminControl.php";
+    require_once "../src/2-controls/mainControl.php";
+    require_once "../src/2-controls/profileControl.php";
+    require_once "../src/2-controls/signinControl.php";
+    require_once "../src/2-controls/signupControl.php";
+    require_once "../src/2-controls/homeControl.php";
+>>>>>>> 015d7af (FUNCIONA LOCAL)
 
 
     /* ----- MODELS ----- */
@@ -17,19 +26,27 @@
 
     
     /* ----- EMESET ----- */
+<<<<<<< HEAD
     // $contenidor = new \Emeset\Contenidor($config);
+=======
+    require_once "../src/4-Emeset/Contenidor.php";
+    require_once "../src/4-Emeset/Peticio.php";
+    require_once "../src/4-Emeset/Resposta.php";
+>>>>>>> 015d7af (FUNCIONA LOCAL)
 
-    // $peticio = $contenidor->peticio();
-    // $resposta = $contenidor->resposta();
+    $contenidor = new \Emeset\Contenidor($config);
+    $peticio = $contenidor->peticio();
+    $resposta = $contenidor->resposta();
 
     
     // /* ----- REQUESTER ----- */
-    if($_REQUEST['r']){
+    if(isset($_REQUEST['r'])){
         $r = $_REQUEST['r'];
     }else{
         $r = "";
     }
 
+<<<<<<< HEAD
     // if($r == "") {
     //     $resposta = homeControl($peticio, $resposta, $contenidor);
     // }
@@ -42,3 +59,20 @@
     // elseif($r === "mainPage"){   
     //     $resposta = mainPageControler();
     // }
+=======
+
+    // /* ----- ROUTER ----- */
+
+    if($r == "") {
+        $resposta = homeControl($peticio, $resposta, $contenidor);
+    }
+    elseif($r === "signUp"){   
+        $resposta = signUpPageControler();
+    }
+    elseif($r === "logOut"){   
+        $resposta = logOutPageControler();
+    }
+    elseif($r === "mainPage"){   
+        $resposta = mainPageControler();
+    }
+>>>>>>> 015d7af (FUNCIONA LOCAL)
