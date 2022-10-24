72f9625e7ab18f1dd78a376ac600ca2b32016c16
Marc Vidal <Marc Vidal Ardevol>
2022-10-24 15:31:26 +0200

    xavi ho soluciona tot

--- public/index.php
+++ public/index.php
@@ -5,20 +5,12 @@
 
 
     /* ----- CONTROLERS ----- */
-<<<<<<< HEAD
-    // require_once "../src/2-controls/adminControl.php";
-    // require_once "../src/2-controls/mainControl.php";
-    // require_once "../src/2-controls/profileControl.php";
-    // require_once "../src/2-controls/signinControl.php";
-    // require_once "../src/2-controls/signupControl.php";
-=======
     require_once "../src/2-controls/adminControl.php";
     require_once "../src/2-controls/mainControl.php";
     require_once "../src/2-controls/profileControl.php";
     require_once "../src/2-controls/signinControl.php";
     require_once "../src/2-controls/signupControl.php";
     require_once "../src/2-controls/homeControl.php";
->>>>>>> 015d7af (FUNCIONA LOCAL)
 
 
     /* ----- MODELS ----- */
@@ -26,13 +18,9 @@
 
     
     /* ----- EMESET ----- */
-<<<<<<< HEAD
-    // $contenidor = new \Emeset\Contenidor($config);
-=======
     require_once "../src/4-Emeset/Contenidor.php";
     require_once "../src/4-Emeset/Peticio.php";
     require_once "../src/4-Emeset/Resposta.php";
->>>>>>> 015d7af (FUNCIONA LOCAL)
 
     $contenidor = new \Emeset\Contenidor($config);
     $peticio = $contenidor->peticio();
@@ -46,24 +34,11 @@
         $r = "";
     }
 
-<<<<<<< HEAD
-    // if($r == "") {
-    //     $resposta = homeControl($peticio, $resposta, $contenidor);
-    // }
-    // elseif($r === "signUp"){   
-    //     $resposta = signUpPageControler();
-    // }
-    // elseif($r === "logOut"){   
-    //     $resposta = logOutPageControler();
-    // }
-    // elseif($r === "mainPage"){   
-    //     $resposta = mainPageControler();
-    // }
-=======
 
     // /* ----- ROUTER ----- */
 
     if($r == "") {
+ 
         $resposta = homeControl($peticio, $resposta, $contenidor);
     }
     elseif($r === "signUp"){   
@@ -75,4 +50,5 @@
     elseif($r === "mainPage"){   
         $resposta = mainPageControler();
     }
->>>>>>> 015d7af (FUNCIONA LOCAL)
+
+    $resposta->resposta();
