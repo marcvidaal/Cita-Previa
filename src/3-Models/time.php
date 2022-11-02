2e0e14eb3aabddb5bd322f6cf46b011a86a3d760
Marc Vidal <Marc Vidal Ardevol>
2022-11-02 16:14:05 +0100

    funciona fa falta carregar defaults

--- src/3-Models/time.php
+++ src/3-Models/time.php
@@ -14,17 +14,24 @@ namespace bd;
 
     public function updateTime($weekdays)
     {
-        $i = 0;
+        $i = 1;
         foreach ($weekdays as $day) {
 
-            $sql = "UPDATE horari_tb SET horari_hora_obert = :horari_hora_obert, horari_hora_tancat = :horari_hora_tancat, horari_tencat = :horari_tencat WHERE horari_dia = :horari_dia";
-            $stmt = $this->sql->prepare($sql);
-            $stmt->bindParam(':horari_hora_obert', $day[start]);
-            $stmt->bindParam(':horari_hora_tancat', $day[end]);
-            $stmt->bindParam(':horari_tencat', $day[closed]);
-            $stmt->bindParam(':horari_dia', array_keys($weekdays)[$i]);
+            $closed = 0;
+            if($day['closed'] === "on"){
+                $closed = 1;
+            }
+            
+            $stmt = $this->sql->prepare('UPDATE horari_tb SET horari_hora_obert = :horari_hora_obert, horari_hora_tencat = :horari_hora_tencat, horari_tencat = :horari_tencat WHERE horari_id = :horari_id');
+            $stmt->execute([':horari_hora_obert' => $day['start'], ':horari_hora_tencat' => $day['end'], ':horari_tencat' => $closed, ':horari_id' => $i]);
             $i = $i + 1;
-            $stmt->execute();
+            
+            die();
+            if ($stmt->errorCode() !== '00000') {
+                $err = $stmt->errorInfo();
+                $code = $stmt->errorCode();
+                die("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
+            }
         }
     }
  }
