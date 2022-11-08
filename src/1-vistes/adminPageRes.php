76380b9b2fc9656e5ad91e3c38e4f9446ede76cf
Marc Vidal <Marc Vidal Ardevol>
2022-11-08 09:33:10 +0100

    funciona amb botstrap

--- src/1-vistes/adminPageRes.php
+++ src/1-vistes/adminPageRes.php
@@ -96,6 +96,7 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.bootstrap5.min.js"></script>
         <script type="text/javascript" src="script.js"></script>
 
 </html>
