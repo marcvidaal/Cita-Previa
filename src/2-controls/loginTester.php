a75be3935a25a38d428c26eb4f60700c6bdebf6c
Marc Vidal <Marc Vidal Ardevol>
2022-11-08 18:56:24 +0100

    ara distingeix usuaris admins de normals

--- src/2-controls/loginTester.php
+++ src/2-controls/loginTester.php
@@ -22,7 +22,12 @@
             if($contrasenya === $usuariDB["client_password"]) {
                 $resposta->setSession("logat", true);
                 $resposta->setSession("user", $user);
-                $resposta->redirect("location: index.php?r=mainPage");
+                if ($usuariDB["client_admin"] == 0) {
+                    $resposta->redirect("location: index.php?r=mainPage");
+                }
+                else{
+                    $resposta->redirect("location: index.php?r=adminPageRes");
+                }
             }
             /* ----- SI L'USUARI EXITEIX A LA BASSE DE DADES I LA CONTRASSENYA ES LA INCORRECTE ----- */
             else {
