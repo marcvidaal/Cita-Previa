135cffd269eb73ef35496320966625c30d292916
Aleix <aleix.fdu@gmail.com>
2022-11-09 16:46:16 +0100

    problemes rebase a develop solved soci

--- src/1-vistes/mainPage.php
+++ src/1-vistes/mainPage.php
@@ -53,25 +53,6 @@
 
 
 
-<<<<<<< HEAD
-                            <div class="row">
-                                <div class="col-sm-10">
-                               
-                                    <?php 
-                                    echo "<table>";
-                                    for ($i=0; $i < $periodesPossibles; $i++) { 
-                                        echo "<tr>";
-                                            for ($j=0; $j < 9; $j++) { ?>
-                                                
-                                                <td>
-
-                                                </td>
-                                                <?php }
-                                    }
-                                    echo "</table>";
-                                    ?>
-
-=======
                         <div class="row">
                                 <div class="col-sm-10">
                                
@@ -99,10 +80,6 @@
 
 
                                                     <?php
-                                                    
-                                                    
-
-                                                    
 
                                                 }
                                                 elseif ($j>0 and $i>0) {
@@ -133,15 +110,7 @@
                                                         else {?>
                                                             <p>no disp</p>
                                                             <?php
-                                                        }
-                                                        
-                                                        // if ($j==) {
-
-                                                        // }
-                                                        // else {
-                                                            
-                                                        // }
-                                                        
+                                                        }                                                        
                                                         ?>
                                                     </form>
                                                 </td>
@@ -158,7 +127,6 @@
 
                                     
 
->>>>>>> 2407cf8 (Afegeix quasi totes les hores a la taula)
                                 </div>
         
                             </div> 
@@ -181,34 +149,6 @@
                                 <p class="m-2">Carril: <?=$entry["carril_numero"];?></p>
                                 </div>
                             <?php } ?>
-
-
-
-                            <!-- <div class="col-sm-3 p-3 m-3 rounded text-light reserves">
-                                <p class="m-2">data de reserva:</p>
-                                <p class="m-2">hora de reserva:</p>
-                                <p class="m-2">carril reservat:</p>
-                            </div>
-                            <div class="col-sm-3 p-3 m-3 rounded text-light reserves">
-                                <p class="m-2">data de reserva:</p>
-                                <p class="m-2">hora de reserva:</p>
-                                <p class="m-2">carril reservat:</p>
-                            </div>
-                            <div class="col-sm-3 p-3 m-3 rounded text-light reserves">
-                                <p class="m-2">data de reserva:</p>
-                                <p class="m-2">hora de reserva:</p>
-                                <p class="m-2">carril reservat:</p>
-                            </div>
-                            <div class="col-sm-3 p-3 m-3 rounded text-light reserves">
-                                <p class="m-2">data de reserva:</p>
-                                <p class="m-2">hora de reserva:</p>
-                                <p class="m-2">carril reservat:</p>
-                            </div> 
-                            <div class="col-sm-3 p-3 m-3 rounded text-light reserves">
-                                <p class="m-2">data de reserva:</p>
-                                <p class="m-2">hora de reserva:</p>
-                                <p class="m-2">carril reservat:</p>
-                            </div>  -->
                         </div>          
                     </div> 
                 </div>
@@ -217,71 +157,3 @@
         <!--Bootstrap JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <div class="col-sm-5 mh-100"> -->
-                        <!--ROW - WELLCOME-->
-                        <!-- <div class="row g-0">
-                            <div class="tod col-sm-12 align-self-start h3">
-                                <P>Good morning,</P>
-                            </div>
-                            <div class="col-sm-12 align-self-start h3">
-                                <p>Marc Vidal</p>
-                            </div>
-                        </div> -->
-                        <!--ROW - FORM-->
-                        <!-- <form method="post" class="row g-0"> -->
-                            <!--COLUMN - FORM CONTAINER -->
-                            <!-- <div class="box col-sm-12 align-self-end rounded p-5"> -->
-                                <!--ROW - TITLE CONTAINER-->
-                                <!-- <div class="row g-0"> -->
-                                    <!--COLUMN - TITLE-->
-                                    <!-- <div class="col-sm-12 text-center mb-5 h4 text-light"> -->
-                                        <!-- Crea la teva reserva -->
-                                    <!-- </div> -->
-                                <!-- </div> -->
-                                <!--ROW - INPUTS CONTAINER-->
-                                <!-- <div class="row g-0 justify-content-between"> -->
-                                    <!--COLUMN - DATE-->
-                                    <!-- <div class="col-sm-5"> -->
-                                        <!-- <input type="date" name="date" class="rounded form-control text-center"> -->
-                                    <!-- </div> -->
-                                    <!--COLUMN - TIME-->
-                                    <!-- <div class="col-sm-5"> -->
-
-                                    <!-- <select name="dropdown" class="rounded w-100 form-control form-control-lg"> -->
-                                            <!-- <option value="1">1</option> -->
-                                        <!-- </select> -->
-                                    <!-- </div> -->
-                                <!-- </div> -->
-                                <!--ROW - INPUTS CONTAINER-->
-                                <!-- <div class="row g-0"> -->
-                                    <!--COLUMN - DROPDOWN-->
-                                    <!-- <div class="col-sm-12 mt-5"> -->
-                                        <!-- <select name="dropdown" class="rounded w-100 form-control form-control-lg"> -->
-                                            <!-- <option value="1">1</option> -->
-                                        <!-- </select> -->
-                                    <!-- </div> -->
-                                <!-- </div> -->
-                                <!--ROW - INPUTS CONTAINER-->
-                                <!-- <div class="row g-0 float-end"> -->
-                                    <!--COLUMN - NEXT-->
-                                    <!-- <div class="col-sm-2 mt-5"> -->
-                                        <!-- <button type="submit" class="rounded btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off"> -->
-                                            <!-- Next -->
-                                        <!-- </button> -->
-                                    <!-- </div> -->
-                                <!-- </div> -->
-                            <!-- </div>  -->
-                        <!-- </form> -->
-                    <!-- </div> -->
--- src/2-controls/reserveControl.php
+++ src/2-controls/reserveControl.php
@@ -7,18 +7,14 @@ function ferReserva($peticio, $resposta, $contenidor){
     //obtenim data introduïda per fer la reserva
     $data = $peticio->get(INPUT_POST, "data");
 
-    
-
     //Array que retorna els dies bloquejats  retorna totes les dates bloquejades
     $diesBloquejats= $usuari->getBlockedDays();
-
 
     foreach($diesBloquejats as $entry) {
         if($data == $entry["dia_bloquejat"]){
             var_dump('this day is not available');
             die();
         }
-        
     }
 
     //Nom dia de la setmana escollit exemple : 2002-04-7 retorna dijous
@@ -28,36 +24,32 @@ function ferReserva($peticio, $resposta, $contenidor){
     $horariEntradaiSortida= $usuari->periodePerDia($nomDiaSetmana);
 
     
-
     //Creem dues variables per guardar el valor de l'entrada i la sortida del dia especificat. Hi guardem el valor recorrent l'array obtingut amb el for each
     $horariEntrada = "";
-    $horarisortida = "";
+    $horariSortida = "";
 
     foreach($horariEntradaiSortida as $entry) {
         $horariEntrada = $entry["horari_hora_obert"];
         $horariSortida = $entry["horari_hora_tencat"];  
     }
     
-    
-
     //Retorna nombre de minuts del periode introduït de la piscina retorna en minuts els periode definit per admin. Ex 30 min per reserva
     $periodeMin = $usuari->getPeriode();
 
-    //Array que retorna els dies bloquejats  retorna totes les dates bloquejades
-    $diesBloquejats= $usuari->getBlockedDays();
+    $minutsTotals=$usuari->diferenciaMinutsEntrePeriode($nomDiaSetmana);
     
-
-
+    //Dividim els minuts totals 
+    $periodesPossibles=$minutsTotals/$periodeMin;
     
     /*
         hi ha dos maneres de ferho. La anterior amb periodesPossibles o amb un while i anant afegint el periode necessari a la hora inici
         pero si no coincideix mai el periode final sempre s'executara.
     */
 
-    
-
     $hores = array();
 
+
+    
 
     for ($i=0; $i <= $periodesPossibles; $i++) { 
         if($i==0){
@@ -68,36 +60,12 @@ function ferReserva($peticio, $resposta, $contenidor){
         }
     }
 
-
-
-    // var_dump($hores);
-    // die();
     $horarisOcupats=$usuari->horariOcupat();
-    
-    // print_r($horarisOcupats);
-    // die();
-    
-    
-
 
     $resposta->set("periodesPossibles", $periodesPossibles);
     $resposta->set("hores", $hores);
     $resposta->set("data", $data);
     $resposta->set("horarisOcupats", $horarisOcupats);
-    
-    
-
-
-
-    // $email = $peticio->getRaw("SESSION", "user");
-
-    
-    
-    
-
-
-
-    $resposta->set("list", $list);
 
     $resposta->setTemplate("mainPage.php");
 
