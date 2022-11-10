109e180e6258c925e8f54d2b0d66975864ab81bb
Marc Vidal <Marc Vidal Ardevol>
2022-11-10 17:23:26 +0100

    actulitzar dades commented

--- src/2-controls/actualitzarDades.php
+++ src/2-controls/actualitzarDades.php
@@ -5,29 +5,22 @@ function actualitzarDades($peticio, $resposta, $contenidor)
     /* ---- ACCES TO DATABASE ----  */
     $usuari = $contenidor->usuari();
 
-
-    
-    $email = $peticio->getRaw("SESSION", "user");
-
+    /* ---- ACCES TO VARIABLES ----  */
     $name = $peticio->get(INPUT_POST, "firstName");
-
     $secondName = $peticio->get(INPUT_POST, "secondName");
     $password = $peticio->get(INPUT_POST, "confirm");
 
+    /* ---- ACCES TO SESIONS ----  */
+    $email = $peticio->getRaw("SESSION", "user");
 
+    /* ---- MODEL FUNCTIONS ----  */
     $usuariDB = $usuari->comprovarCompteUsuari($email);
 
+    if ($name != "") {$usuari->updateFirstName($email, $name);}
+    if ($secondName != "") {$usuari->updateSecondName($email, $secondName);}
+    if ($password != "") {$usuari->updatePassword($email, $password);}
 
-    if ($name != "") {
-        $usuari->updateFirstName($email, $name);
-    }
-    if ($secondName != "") {
-        $usuari->updateSecondName($email, $secondName);
-    }
-    if ($password != "") {
-        $usuari->updatePassword($email, $password);
-    }
-
+    /* ---- REDIRECTS ----  */
     if ($usuariDB["client_admin"] == 0) {
         $resposta->redirect("location: index.php?r=mainPage");
     } else {
