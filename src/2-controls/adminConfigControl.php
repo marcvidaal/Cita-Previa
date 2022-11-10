eef67a1e25b666b4d94e2fe68fa00bbf0ccca8db
Marc Vidal <Marc Vidal Ardevol>
2022-11-10 17:31:27 +0100

    adminConfigControl commented

--- src/2-controls/adminConfigControl.php
+++ src/2-controls/adminConfigControl.php
@@ -5,10 +5,13 @@ function adminConfigControl($peticio, $resposta, $contenidor)
     /* ---- ACCES TO DATABASE ----  */
     $time = $contenidor->time();
 
-    
+    /* ---- MODEL FUNCTIONS ----  */
     $times = $time->pullTime();
 
+    /* ---- ACCES TO VARIABLES IN VEWS ----  */
     $resposta->set("times", $times);
+    
+    /* ---- REDIRECTS ----  */
     $resposta->setTemplate("adminPageConfig.php");
 
     return $resposta;
