38eefec0a2406b01e4afc1ce500bed7cd938f79a
Marc Vidal <Marc Vidal Ardevol>
2022-11-10 17:30:01 +0100

    adminBlockControl commented

--- src/2-controls/adminBlockControl.php
+++ src/2-controls/adminBlockControl.php
@@ -5,11 +5,13 @@ function adminBlockControl($peticio, $resposta, $contenidor)
     /* ---- ACCES TO DATABASE ----  */
     $admin = $contenidor->admin();
 
-
-    
+    /* ---- MODEL FUNCTIONS VARIABLES ----  */
     $dates = $admin->llistBlockDates();
 
+    /* ---- ACCES TO VARIABLES IN VEWS ----  */
     $resposta->set("dates", $dates);
+
+    /* ---- REDIRECTS ----  */
     $resposta->setTemplate("adminPageBlock.php");
 
     return $resposta;
