209fbef659868136670d8d6073d4b493d11fb391
Marc Vidal <Marc Vidal Ardevol>
2022-11-11 16:01:53 +0100

    model estroctura

--- src/3-Models/connexio.php
+++ src/3-Models/connexio.php
@@ -1,28 +1,36 @@
 <?php
+/**
+ * CITA PREVIA.
+ *
+ * @author: marc vidal ardevol marcvidaal5@gmail.com
+ *
+ * model que gestiona la conexio a la base de dades.
+ **/
 
 namespace Daw;
 
-    class connexio {
+class connexio
+{
 
-        public $sql = null;
+    public $sql = null;
 
-        public function __construct($config)
-        {
-            
-            //$dsn = "mysql:host={$config['host']};dbname={$config['dbname']}";
-            $usuari = $config["user"];
-            $clau = $config["pass"];
-            $dsn =  "mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=cita_previa;";
-            
-            try {
+    public function __construct($config)
+    {
+
+        //$dsn = "mysql:host={$config['host']};dbname={$config['dbname']}";
+        $usuari = $config["user"];
+        $clau = $config["pass"];
+        $dsn =  "mysql:unix_socket=/Applications/MAMP/tmp/mysql/mysql.sock;dbname=cita_previa;";
+
+        try {
             $this->sql = new \PDO($dsn, $usuari, $clau);
-
-            } catch (PDOException $e) {
-                die('Ha fallat la connexió: ' . $e->getMessage());
-            }
+        } catch (\PDOException $e) {
+            die('Ha fallat la connexió: ' . $e->getMessage());
         }
+    }
 
-        public function getConnection(){
-            return $this->sql;
+    public function getConnection()
+    {
+        return $this->sql;
     }
 }
--- src/3-Models/time.php
+++ src/3-Models/time.php
@@ -1,52 +1,57 @@
 <?php
+/**
+ * CITA PREVIA.
+ *
+ * @author: marc vidal ardevol marcvidaal5@gmail.com
+ *
+ * model que gestiona totes les peticions de la pagina adinistrado config.
+ **/
+namespace bd;
 
-    namespace bd;
+class time
+{
 
-    class time{
-    
-        public $sql = null;
+    public $sql = null;
 
-        /* ----- CRIDEM LA CONEXIO A LA BASSE DE DADES ----- */
-        public function __construct($connexioDB)
-        {
-            $this->sql = $connexioDB->getConnection();
-        }
-        
-        /* ----- RETORNA ELS VALORS DE LA BASSE DE DADES I ELS GUARDA A UN ARRAY ----- */
-        public function pullTime()
-        {
-            $stmt = $this->sql->prepare('SELECT * FROM horari_tb');
-            $stmt->execute();
-            /* ----- GUARRDEM TOT LA INFO DINS DE L'ARRAY TIMES ----- */
-            $times = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    /* ----- CRIDEM LA CONEXIO A LA BASSE DE DADES ----- */
+    public function __construct($connexioDB)
+    {
+        $this->sql = $connexioDB->getConnection();
+    }
 
-            return $times;
-        }
+    /* ----- RETORNA ELS VALORS DE LA BASSE DE DADES I ELS GUARDA A UN ARRAY ----- */
+    public function pullTime()
+    {
+        $stmt = $this->sql->prepare('SELECT * FROM horari_tb');
+        $stmt->execute();
+        /* ----- GUARRDEM TOT LA INFO DINS DE L'ARRAY TIMES ----- */
+        $times = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
-        /* ----- ACTUALITZA ELS TEMPS DE LA BASSE DE DADES AMB ELS NOUS INTRODUITS ----- */
-        public function updateTime($weekdays)
-        {
-            $i = 1;
-            foreach ($weekdays as $day) {
+        return $times;
+    }
 
-                $closed = 0;
-                if($day['closed'] === "on"){
-                    $closed = 1;
-                    $day['start'] = "00:00:00";
-                    $day['end'] = "00:00:00";
-                    
-                }
-                
-                $stmt = $this->sql->prepare('UPDATE horari_tb SET horari_hora_obert = :horari_hora_obert, horari_hora_tencat = :horari_hora_tencat, horari_tencat = :horari_tencat WHERE horari_id = :horari_id');
-                $stmt->execute([':horari_hora_obert' => $day['start'], ':horari_hora_tencat' => $day['end'], ':horari_tencat' => $closed, ':horari_id' => $i]);
-                $i = $i + 1;
-                
-                if ($stmt->errorCode() !== '00000') {
-                    $err = $stmt->errorInfo();
-                    $code = $stmt->errorCode();
-                    die("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
-                }
+    /* ----- ACTUALITZA ELS TEMPS DE LA BASSE DE DADES AMB ELS NOUS INTRODUITS ----- */
+    public function updateTime($weekdays)
+    {
+        $i = 1;
+        foreach ($weekdays as $day) {
+
+            $closed = 0;
+            if ($day['closed'] === "on") {
+                $closed = 1;
+                $day['start'] = "00:00:00";
+                $day['end'] = "00:00:00";
+            }
+            $query = 'UPDATE horari_tb SET horari_hora_obert = :horari_hora_obert, horari_hora_tencat = :horari_hora_tencat, horari_tencat = :horari_tencat WHERE horari_id = :horari_id';
+            $stmt = $this->sql->prepare($query);
+            $stmt->execute([':horari_hora_obert' => $day['start'], ':horari_hora_tencat' => $day['end'], ':horari_tencat' => $closed, ':horari_id' => $i]);
+            $i = $i + 1;
+
+            if ($stmt->errorCode() !== '00000') {
+                $err = $stmt->errorInfo();
+                $code = $stmt->errorCode();
+                die("Error.   {$err[0]} - {$err[1]}\n{$err[2]} $query");
             }
         }
-
     }
+}
